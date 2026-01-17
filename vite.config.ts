91453c2508d63bdc4cff3b7c983eb667052470bd
DEDY MEYGA SAPUTRA <119374110+metropoliz00@users.noreply.github.com>
2026-01-18 00:32:53 +0700

    q

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Sangat penting untuk deployment cPanel agar path file tidak pecah
+  // Base harus sesuai dengan nama folder di server-dir (deploy.yml)
+  // Jika URL webnya arsip.uptsdnremen2.sch.id/arsip/ maka base: '/arsip/'
+  base: '/arsip/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
